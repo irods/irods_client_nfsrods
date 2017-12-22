c420a1633af68408dd1c4baad81eedeee397659a
Mike Conway <mike.conway@nih.gov>
2017-12-22 10:35:16 -0500

    #1 resolve log4j and first test

--- irods-vfs-impl/src/test/java/org/irods/jargon/nfs/vfs/IrodsVirtualFileSystemTest.java
+++ irods-vfs-impl/src/test/java/org/irods/jargon/nfs/vfs/IrodsVirtualFileSystemTest.java
@@ -1,14 +1,64 @@
 package org.irods.jargon.nfs.vfs;
 
-import static org.junit.Assert.*;
+import java.util.Properties;
 
+import org.irods.jargon.core.connection.IRODSAccount;
+import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
+import org.irods.jargon.core.pub.IRODSFileSystem;
+import org.irods.jargon.core.pub.io.IRODSFile;
+import org.irods.jargon.core.utils.MiscIRODSUtils;
+import org.irods.jargon.testutils.AssertionHelper;
+import org.irods.jargon.testutils.IRODSTestSetupUtilities;
+import org.irods.jargon.testutils.TestingPropertiesHelper;
+import org.irods.jargon.testutils.filemanip.ScratchFileUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class IrodsVirtualFileSystemTest {
 
+	private static Properties testingProperties = new Properties();
+	private static TestingPropertiesHelper testingPropertiesHelper = new TestingPropertiesHelper();
+	private static ScratchFileUtils scratchFileUtils = null;
+	public static final String IRODS_TEST_SUBDIR_PATH = "IrodsVirtualFileSystemTest";
+	private static IRODSTestSetupUtilities irodsTestSetupUtilities = null;
+	private static AssertionHelper assertionHelper = null;
+	private static IRODSFileSystem irodsFileSystem;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		TestingPropertiesHelper testingPropertiesLoader = new TestingPropertiesHelper();
+		testingProperties = testingPropertiesLoader.getTestProperties();
+		scratchFileUtils = new ScratchFileUtils(testingProperties);
+		scratchFileUtils.clearAndReinitializeScratchDirectory(IRODS_TEST_SUBDIR_PATH);
+		irodsTestSetupUtilities = new IRODSTestSetupUtilities();
+		irodsTestSetupUtilities.initializeIrodsScratchDirectory();
+		irodsTestSetupUtilities.initializeDirectoryForTest(IRODS_TEST_SUBDIR_PATH);
+		assertionHelper = new AssertionHelper();
+		irodsFileSystem = IRODSFileSystem.instance();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		irodsFileSystem.closeAndEatExceptions();
+	}
+
+	@After
+	public void afterEach() throws Exception {
+		irodsFileSystem.closeAndEatExceptions();
+	}
+
 	@Test
-	public void testIrodsVirtualFileSystem() {
-		fail("Not yet implemented");
+	public void testIrodsVirtualFileSystem() throws Exception {
+		IRODSAccount irodsAccount = testingPropertiesHelper.buildIRODSAccountFromTestProperties(testingProperties);
+		IRODSAccessObjectFactory accessObjectFactory = irodsFileSystem.getIRODSAccessObjectFactory();
+		String homeDir = MiscIRODSUtils.buildIRODSUserHomeForAccountUsingDefaultScheme(irodsAccount);
+		IRODSFile rootFile = accessObjectFactory.getIRODSFileFactory(irodsAccount).instanceIRODSFile(homeDir);
+		IrodsVirtualFileSystem vfs = new IrodsVirtualFileSystem(accessObjectFactory, irodsAccount, rootFile);
+		Assert.assertNotNull("no vfs created", vfs);
+
 	}
 
 }
