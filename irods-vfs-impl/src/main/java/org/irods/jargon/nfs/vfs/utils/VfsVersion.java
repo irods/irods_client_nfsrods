6615ce610168ebedccc4771c1336cab8586895bf
Alek M <amieczko@uncc.edu>
2018-08-09 15:04:55 -0400

    Working Example for multiuser NFS

--- irods-vfs-impl/src/main/java/org/irods/jargon/nfs/vfs/utils/VfsVersion.java
+++ irods-vfs-impl/src/main/java/org/irods/jargon/nfs/vfs/utils/VfsVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.nfs.vfs.utils;
 public final class VfsVersion {
  public static String VERSION="1.0.0-SNAPSHOT";
- public static String BUILD_TIME="2018-08-07T20:14:30Z";
+ public static String BUILD_TIME="2018-08-09T19:02:04Z";
 }
