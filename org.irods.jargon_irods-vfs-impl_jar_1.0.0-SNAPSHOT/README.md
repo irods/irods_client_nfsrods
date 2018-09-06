ff6c5d72b1767c9b9f30be1c456a87a2a1c02945
Kory Draughn <korydraughn@renci.org>
2018-09-06 11:58:53 -0400

    Removed old files.

--- org.irods.jargon_irods-vfs-impl_jar_1.0.0-SNAPSHOT/README.md
+++
@@ -1,6 +0,0 @@
-# iRODS NFS4J Virtual File System implementation
-
-## A prototype of an iRODS plugin to NFS4J: https://github.com/dCache/nfs4j
-
-
-
--- org.irods.jargon_irods-vfs-impl_jar_1.0.0-SNAPSHOT/src/main/java/org/irods/jargon/nfs/vfs/utils/VfsVersion.java
+++
@@ -1,5 +0,0 @@
-package org.irods.jargon.nfs.vfs.utils;
-public final class VfsVersion {
- public static String VERSION="1.0.0-SNAPSHOT";
- public static String BUILD_TIME="20180620-1229";
-}
