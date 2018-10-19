a7f9619649b7d228d85278eade25180c1add0860
Kory Draughn <korydraughn@renci.org>
2018-10-19 01:47:53 -0400

    Created Docker image that compiles and runs iRODS/Postgres.

--- irods-vfs-impl/start.sh
+++ irods-vfs-impl/start.sh
@@ -2,9 +2,40 @@
 
 # Start the Postgres database.
 service postgresql start
-seconds=10
-echo "Sleeping for $seconds seconds to let the database complete start up ..."
-sleep $seconds
+
+# Clone iRODS repo and initialize.
+git clone https://github.com/korydraughn/irods
+cd irods
+git checkout re_plugin_issue_4144
+git submodule update --init
+cd /
+
+# Build iRODS.
+mkdir irods_build
+cd irods_build
+cmake -GNinja /irods
+ninja package
+
+# Install new irods-dev package.
+dpkg -i irods-{runtime,dev}*.deb
+
+# Clone iCommands repo.
+cd /
+git clone https://github.com/korydraughn/irods_client_icommands
+cd irods_client_icommands
+git checkout 4-2-stable
+cd /
+
+# Build and install iCommands.
+mkdir icommands_build
+cd icommands_build
+cmake -GNinja /irods_client_icommands
+ninja package
+dpkg -i irods-*.deb
+
+# Install remaining iRODS packages.
+cd /irods_build
+dpkg -i irods-{server,database-plugin-postgres}*.deb
 
 # Set up iRODS.
 python /var/lib/irods/scripts/setup_irods.py < /var/lib/irods/packaging/localhost_setup_postgres.input
