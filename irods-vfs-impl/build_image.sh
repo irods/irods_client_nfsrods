e28d085d5102745fc63a7225c8f04029e8be835e
Kory Draughn <korydraughn@renci.org>
2018-11-09 14:05:59 -0500

    Reorganized project structure for testing. Added testing via docker.

--- irods-vfs-impl/build_image.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-docker build -t irods_ub16_postgres .
--- irods-vfs-impl/launch_container.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-irods_port=${1:-9000}
-
-docker run -d --rm --name irods_ub16_postgres -p $irods_port:1247 irods_ub16_postgres
