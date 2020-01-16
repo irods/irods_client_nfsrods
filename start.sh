91a72d8b953ecc02c25d78078d5529e3b811b3d3
Terrell Russell <terrellrussell@gmail.com>
2020-01-15 21:23:30 -0500

    [#76] bump version to 0.9.3

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-java -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar /irods_client_nfsrods/irods-vfs-impl/target/nfsrods-0.9.2-jar-with-dependencies.jar "$@"
+java -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar /irods_client_nfsrods/irods-vfs-impl/target/nfsrods-0.9.3-jar-with-dependencies.jar "$@"
