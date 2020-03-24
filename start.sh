3ef1bc5ddc4f1855d00ab314fb3f0107f0f52508
Terrell Russell <terrellrussell@gmail.com>
2020-03-23 20:55:03 -0400

    [#56] add SSL support
    The container will now load /nfsrods_ssl.crt, if present, into the OpenJDK keystore.

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
 
+if [ -f /nfsrods_ssl.crt ];
+then
+   echo "Cert found for NFSRODS"
+   set +e
+   keytool -delete -noprompt -alias mycert -keystore /etc/ssl/certs/java/cacerts -storepass changeit
+   set -e
+   echo "Importing cert to OpenJDK keystore"
+   keytool -import -trustcacerts -keystore /etc/ssl/certs/java/cacerts -storepass changeit -noprompt -alias mycert -file /nfsrods_ssl.crt
+   echo "Done"
+else
+   echo "Cert not found for NFSRODS - not importing"
+fi
+
 java -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar /irods_client_nfsrods/irods-vfs-impl/target/nfsrods-0.9.3-jar-with-dependencies.jar "$@"
