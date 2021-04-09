f4a5ce2323ab8482276ed3f1dbd78cf4ec4cb7f0
Kory Draughn <korydraughn@renci.org>
2021-04-09 09:24:15 -0400

    [#123] Prints only the SHA information on request.

--- start.sh
+++ start.sh
@@ -1,18 +1,21 @@
 #! /bin/bash
 
-if [ -f /nfsrods_ssl.crt ];
-then
-   echo "Cert found for NFSRODS"
-   set +e
-   keytool -delete -noprompt -alias mycert -keystore /etc/ssl/certs/java/cacerts -storepass changeit
-   set -e
-   echo "Importing cert to OpenJDK keystore"
-   keytool -import -trustcacerts -keystore /etc/ssl/certs/java/cacerts -storepass changeit -noprompt -alias mycert -file /nfsrods_ssl.crt
-   echo "Done"
-else
-   echo "Cert not found for NFSRODS - not importing"
-fi
+if [ "$1" != "sha" ]; then
+    # Handle SSL/TLS cert.
+    if [ -f /nfsrods_ssl.crt ]; then
+       echo "Cert found for NFSRODS"
+       set +e
+       keytool -delete -noprompt -alias mycert -keystore /etc/ssl/certs/java/cacerts -storepass changeit
+       set -e
+       echo "Importing cert to OpenJDK keystore"
+       keytool -import -trustcacerts -keystore /etc/ssl/certs/java/cacerts -storepass changeit -noprompt -alias mycert -file /nfsrods_ssl.crt
+       echo "Done"
+    else
+       echo "Cert not found for NFSRODS - not importing"
+    fi
 
-service rpcbind start
+    # Start services.
+    service rpcbind start
+fi
 
 java -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar /irods_client_nfsrods/irods-vfs-impl/target/nfsrods-1.0.1-jar-with-dependencies.jar "$@"
