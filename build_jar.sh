8c686ca0f5a1bf5da0dfa0a9443036d650a56402
Kory Draughn <korydraughn@ymail.com>
2022-10-12 08:50:11 -0400

    [#169] Improved support for automated builds.
    For environments where there is no TTY device, build_jar.sh can be
    passed --non-interactive. This instructs the script to launch a docker
    container without the -i and -t options.

--- build_jar.sh
+++ build_jar.sh
@@ -1,11 +1,34 @@
 #! /bin/bash
 
+usage() {
+cat <<_EOF_
+USAGE: bash build_jar.sh [OPTIONS]
+
+Builds a runnable NFSRODS JAR file based on the currently checked out source.
+
+Options:
+  --non-interactive   Launches docker container without -i and -t.
+  -h, --help          Shows this message.
+_EOF_
+    exit
+}
+
+docker_run_iteractive_options="-it"
+
+while [ -n "$1" ]; do
+    case "$1" in
+        --non-interactive)  shift; unset docker_run_iteractive_options;;
+        -h|--help)          usage;;
+    esac
+    shift
+done
+
 # Compiles an executable JAR file from the local repository and
 # stores all build artifacts in local_maven_repo.
 #
 # The local_maven_repo directory keeps your personal maven repository
 # (i.e. $HOME/.m2) clean and safe from the "root" user.
-docker run -it --rm --name nfsrods_builder \
+docker run ${docker_run_iteractive_options} --rm --name nfsrods_builder \
     -v $PWD/local_maven_repo:/root/.m2 \
     -v $PWD:/irods_client_nfsrods \
     -w /irods_client_nfsrods \
