24a598bf0c937a2a4fda03edb8eda10f1d4ef1b6
Kory Draughn <korydraughn@renci.org>
2023-11-21 15:08:06 -0500

    [#193,#196] Use -cacerts and rename alias to "nfsrods".

--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@ if [ "$1" != "sha" ]; then
     if [ -f /nfsrods_ssl.crt ]; then
        echo "Cert found for NFSRODS"
        set +e
-       keytool -delete -noprompt -alias mycert -keystore /etc/ssl/certs/java/cacerts -storepass changeit
+       keytool -delete -noprompt -alias nfsrods -cacerts -storepass changeit
        set -e
        echo "Importing cert to OpenJDK keystore"
-       keytool -import -trustcacerts -keystore /etc/ssl/certs/java/cacerts -storepass changeit -noprompt -alias mycert -file /nfsrods_ssl.crt
+       keytool -import -trustcacerts -cacerts -storepass changeit -noprompt -alias nfsrods -file /nfsrods_ssl.crt
        echo "Done"
     else
        echo "Cert not found for NFSRODS - not importing"
